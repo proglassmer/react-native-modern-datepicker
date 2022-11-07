af205797cfdafdfe280109336746ab4a00fe3960
thomaddev <thomad.dev@gmail.com>
2022-11-07 11:51:53 +0700

    update seelctmonth lib

--- src/datePicker/components/SelectMonth.js
+++ src/datePicker/components/SelectMonth.js
@@ -54,7 +54,7 @@ const SelectMonth = () => {
   const onSelectMonth = month => {
     if (show) {
       let y = Number(utils.toEnglish(year)) - 543;
-      const date = utils.getDate(utils.validYear(mainState.activeDate, (y - 543)));
+      const date = utils.getDate(utils.validYear(mainState.activeDate, y));
       const activeDate =
         month !== null ? (isGregorian ? date.month(month) : date.jMonth(month)) : date;
       setMainState({
